a4546f3e6f3e030b5cda72106d8905060e1e450f
hasan <akonhasan680@gmail.com>
2023-05-18 16:41:16 +0600

    error page set up

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -6,7 +6,22 @@ const ErrorPage = () => {
     const { error, status } = useRouteError()
     UseTitle('error')
     return (
-       
+      <section className='container  w-full mx-auto   bg-gray-100 text-gray-900'>
+      <img className=' w-full mx-auto my-auto' src="https://i.ibb.co/Qvzq3gT/404-landing-page-free-vector.webp" alt="" />
+           <div className='text-center '>
+             
+             <p className='text-2xl font-semibold md:text-3xl mb-8'>
+               {error?.message}
+             </p>
+             <Link
+               to='/'
+               className='px-8 py-3 font-semibold rounded bg-orange-400 text-gray-900'
+             >
+               Back to homepage
+             </Link>
+           </div>
+         
+       </section> 
     );
 };
 
